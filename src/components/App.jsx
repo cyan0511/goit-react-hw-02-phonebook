bdd69a426c77cd2975842dc87ed7cfb90616c3dc
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-06-06 21:04:52 +0800

    goit-react-hw-02-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,7 +44,7 @@ export class App extends Component {
   };
 
   render() {
-    const { contacts, filter, findBy, number } = this.state;
+    const { contacts, filter, findBy } = this.state;
     const contactList = contacts
       .filter(c => findBy === '' ||  c[findBy].toLowerCase().includes(filter));
 
